c80b6856abf8d123fe2a2cdee43dac0b98f175b2
varija <varijabalumuri8@gmail.com>
2025-06-03 12:28:57 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed("#typed-text", {
+        strings: ["Java Developer", "Spring Boot Enthusiast", "AWS Certified Engineer"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    });
+});
